750ee8dc2fe84a6fd3ec3e719f3b5dc77e363142
Sanghun <po4865@gmail.com>
2025-04-16 11:15:57 +0200

    chore(main): sketch taking car input in main() in comments

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,17 @@
 package racingcar
 
 fun main() {
-    // TODO: Implement the program
+    // TODO: Implement input process
+
+    // Take car names from user input -> list
+    // Use Console api
+
+    // Separate string with ','
+
+    // Validate input
+
+    // Create new Car() instance
+
+    // Create list of Car()
+
 }
