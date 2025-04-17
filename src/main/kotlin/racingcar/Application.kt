4061da07f36eb6fa3e5c259490bbb9cd77ea341d
Sanghun <po4865@gmail.com>
2025-04-17 09:51:27 +0200

    chore(main): sketch how the program will run at main() in comments

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,5 +6,19 @@ fun main() {
         Car(name)
     }
 
+    // Read round count from user input
+
+    // Create and initialize an object 'GameMaster'
+
+    // Start game
+
+    // Iterate through rounds
+
+        // Show the result of round
+
+        // Check the round count
+
+    // Show winner(s)
+
     return
 }
