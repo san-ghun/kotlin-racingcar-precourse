ef4ddcb9f8b5ac5a1fecff410a285f824f5740cc
Sanghun <po4865@gmail.com>
2025-04-17 16:25:04 +0200

    test(InputViewTest): add test methods for 'readRoundCount()'
    Add test methods for 'readRoundCount()' including, 'convertRoundCount()' and 'validateRoundCount()' to test throwing exceptions on invalid inputs and numbers. And also test method with valid inputs.

--- src/test/kotlin/racingcar/InputViewTest.kt
+++ src/test/kotlin/racingcar/InputViewTest.kt
@@ -2,6 +2,8 @@ package racingcar
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.assertThrows
 
 class InputViewTest {
     @Test
@@ -22,16 +24,38 @@ class InputViewTest {
 
     @Test
     fun `throw exception if the number of rounds is not a number`() {
-
+        val invalidInputs = listOf("", " ", "a", "race", "  b  ", "3333333333", "3.14")
+        invalidInputs.forEach { input ->
+            val exception = assertThrows<IllegalArgumentException> {
+                InputView.convertRoundCount(input)
+            }
+            assertEquals(
+                "The number of rounds should be a number.",
+                exception.message
+            )
+        }
     }
 
     @Test
     fun `throw exception if the number of rounds is less than or equal to 0`() {
-
+        val invalidNumbers = listOf(0, -1, -2147483647)
+        invalidNumbers.forEach { number ->
+            val exception = assertThrows<IllegalArgumentException> {
+                InputView.validateRoundCount(number)
+            }
+            assertEquals(
+                "The number of rounds must be a number greater than or equal to 1.",
+                exception.message
+            )
+        }
     }
 
     @Test
     fun `pass with the valid number of rounds`() {
-
+        val inputs = listOf("1", "12", "  3", "4  ", "  5  ", "01", "0012", "2147483647")
+        inputs.forEach { input ->
+            val count = InputView.convertRoundCount(input.trim())
+            InputView.validateRoundCount(count)
+        }
     }
 }
