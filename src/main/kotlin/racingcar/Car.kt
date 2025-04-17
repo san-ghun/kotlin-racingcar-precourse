b7b8d0f197a65625b1c2cb17bc257a03601e48e1
Sanghun <po4865@gmail.com>
2025-04-17 20:57:17 +0200

    feat(Car): implement 'moveForward()' and 'getPosition()' methods in 'Car()'
    Car.kt
    - Add new method 'moveForward()' to increase position of car based on the random value as an argument.
    - Add new method 'getPosition()' to return 'position' of a 'Car()' instance.
    
    BREAKING CHANGE: alter a public immutable property 'position' of 'Car()' from 'val' to 'private var' which is a private mutable property to prevent direct access to the 'position' value, and rather access and manipulate via internal methods.

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,10 +1,16 @@
 package racingcar
 
-class Car(val name: String, val position: Int = 0) {
+class Car(val name: String, private var position: Int = 0) {
 
     init {
         require(name.length in 1..5) {
             "The car name must be at least 1 character and no more than 5 characters. Input value: '$name'"
         }
     }
+
+    fun moveForward(randomValue: Int) {
+        if (randomValue >= 4) { position++ }
+    }
+
+    fun getPosition(): Int = position
 }
