ad169943148fbb2eb4e15d47586edf0530cf8737
Sanghun <po4865@gmail.com>
2025-04-18 15:46:54 +0200

    test: add test method in a new test file 'RandomValueGeneratorTest.kt'
    - Add a test method to test the method '.generateNumber()' generate random number between 0 and 9, through 100 iteration.

---
+++ src/test/kotlin/racingcar/RandomValueGeneratorTest.kt
@@ -0,0 +1,16 @@
+package racingcar
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.assertThrows
+
+class RandomValueGeneratorTest {
+    @Test
+    fun `generate random number between 0 and 9`() {
+        val values = (1..100).map { RandomValueGenerator.generateNumber() }
+        values.forEach { value ->
+            assertEquals(true, (value in 0..9))
+        }
+    }
+}
