9bf477e32467b3fe104c2aaae99a2b672c13992c
Sanghun <po4865@gmail.com>
2025-04-19 17:38:11 +0200

    refactor(RandomValueGenerator): add new interface 'ValueGenerator' to inherit and unify method
    With one 'RandomValueGenerator' make it hard to test the 'RacingManger' and Application test. Unifying the structure of object into a interface 'ValueGenerator' will make the application and 'RacingManager' extendable by new value generator in future. With this inheritance, it is available to extensive test code.

--- src/main/kotlin/racingcar/RandomValueGenerator.kt
+++ src/main/kotlin/racingcar/RandomValueGenerator.kt
@@ -2,6 +2,18 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Randoms
 
-object RandomValueGenerator {
-    fun generateNumber(): Int = Randoms.pickNumberInRange(0, 9)
+interface ValueGenerator {
+    fun generateNumber(): Int
+}
+
+object RandomValueGenerator : ValueGenerator{
+    override fun generateNumber(): Int = Randoms.pickNumberInRange(0, 9)
+}
+
+object TestMoveValueGenerator : ValueGenerator{
+    override fun generateNumber(): Int = 9
+}
+
+object TestDoNotMoveValueGenerator : ValueGenerator{
+    override fun generateNumber(): Int = 0
 }
