a4f0ce441a9d465f2e759564235ff42994622147
Sanghun <po4865@gmail.com>
2025-04-19 20:35:08 +0200

    feat(ResultView): add new object 'ResultView' with methods to print results
    Add new object 'ResultView' in 'ResultView.kt' file, which has methods for printing out result of each round and final winners.

---
+++ src/main/kotlin/racingcar/ResultView.kt
@@ -0,0 +1,14 @@
+package racingcar
+
+object ResultView {
+    fun printRace(cars: List<Car>) {
+        cars.forEach { car ->
+            println("${car.name} : ${car.drawPosition()}")
+        }
+        println()
+    }
+
+    fun printWinners(winners: List<Car>) {
+        println("Winners : ${winners.joinToString { it.name }}")
+    }
+}
