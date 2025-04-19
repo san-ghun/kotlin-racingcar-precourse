ceba2fa8fd39e824c36d6ea030c9c771756ac675
Sanghun <po4865@gmail.com>
2025-04-19 22:26:40 +0200

    chore(main): remove pseudocode with comment and apply 'ResultView' in 'main()'

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,36 +5,15 @@ fun main() {
     val cars = carNames.map { name ->
         Car(name)
     }
-
     val roundCount = InputView.readRoundCount()
-
-    // Create and initialize an object 'RacingManager'
     val manager = RacingManager(cars, roundCount)
-
-    // Start game
-    println()
-
-    // Iterate through rounds
+    println("\nRace Results")
     while (manager.isRacing()) {
-
-        // Move all cars
         manager.moveAllCars()
-
-        // Show the result of round
-        //ResultView.printRace(cars)
-        cars.forEach { car ->
-            println("${car.name} : ${car.drawPosition()}")
-        }
-        println()
-
-        // Go next round
+        ResultView.printRace(manager.cars)
         manager.goNextRound()
     }
-
-    // Show winner(s)
     val winners = manager.findWinners()
-    //ResultView.printWinners(winners)
-    println("Winners : ${winners.joinToString { it.name }}")
-
+    ResultView.printWinners(winners)
     return
 }
