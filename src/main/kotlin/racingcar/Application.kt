cc73c6f1ed2e7a5f667ef0f7a2a625983ed59654
Sanghun <po4865@gmail.com>
2025-05-02 15:57:02 +0200

    chore(main): add pseudocode with comments

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,26 @@
 package racingcar
 
 fun main() {
-    // TODO: Implement the program
+    // 1. take user input car names
+        // validate input
+        // comma-separation
+        // less than 5 char, inclusive
+
+    // 2. take user input number of rounds
+        // validate input
+        // type integer
+
+    // 3. initiate racing master
+
+    // 4. display start of game
+
+    // 5. start racing
+        // 1. iterate or repeat number of rounds
+        // 2. car progress based on the rand-gen number
+        // 3. display result of the round
+
+    // 6. determine winners
+
+    // 7. display winners
+        // comma-separation
 }
