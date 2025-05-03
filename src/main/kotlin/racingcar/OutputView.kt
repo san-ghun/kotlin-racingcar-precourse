cb7a5f51447cf49b4e041fd80e6fd9c5a5d36ce2
Sanghun <po4865@gmail.com>
2025-05-03 16:40:39 +0200

    feat(OutputView): add 'showRoundResult()' method

--- src/main/kotlin/racingcar/OutputView.kt
+++ src/main/kotlin/racingcar/OutputView.kt
@@ -2,7 +2,9 @@ package racingcar
 
 object OutputView {
     fun showRoundResult(cars: List<Car>) {
-
+        cars.forEach { car ->
+            println("${car.getName()} : ${car.displayProgress()}")
+        }
     }
 
     fun showWinners(winners: List<Car>) {
